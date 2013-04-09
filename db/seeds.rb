2edd5a6651e7fe4944db3371a511ac3805996462
david <davidqhr@gmail.com>
2013-04-09 16:24:38 +0800

    add seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Game.create(name: "5-10-K", url: "5-10-K", description: "5-10-K")
+Game.create(name: "Matching Game", url: "matching-game", description: "matching-game")
+Game.create(name: "Hearts", url: "hearts", description: "hearts")
